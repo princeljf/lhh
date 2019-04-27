1c07eb560996c55d272a095a7b0368ae72a8a763
lhhformac <384234884@qq.com>
2019-04-27 21:00:56 +0800

    lhh__pyspider

---
+++ pyspider/tusharePro/tusharePro.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# Created on 2019-04-27 14:50:51
+# Project: tusharePro
+
+from pyspider.libs.base_handler import *
+
+###获取两个字符串中间的值
+def getStrBetween(str="",startStr="",endStr=""):
+    sLength = len(startStr)
+    eLength = len(endStr)
+    if(sLength>0):
+        startIndex = str.find(startStr) + sLength
+    else:
+        startIndex = 0
+    if(eLength>0):
+        endIndex = str.find(endStr)
+    else:
+        endIndex = len(str)
+    result = str[startIndex:endIndex]
+    return result;
+###获取输入参数/输出参数
+def getTable2tushare(pyQueryTable, fields):
+    pqTable = pyQueryTable
+    inputfield = fields
+    inputArr = []
+    for i in range(pqTable('tbody tr').length):
+        tds = pqTable('tbody tr').eq(i)('td')
+        index = 0
+        item = {}
+        for key in inputfield['keys']:
+            item[key] = tds.eq(index).text()
+            index+=1
+        inputArr.append(item)
+    return inputArr
+
+
+class Handler(BaseHandler):
+    crawl_config = {
+    }
+
+    @every(minutes=24 * 60)
+    def on_start(self):
+        self.crawl('https://tushare.pro/document/2', callback=self.index_page)
+
+    @config(age=10 * 24 * 60 * 60)
+    def index_page(self, response):
+        for each in response.doc('a[href^="https://tushare.pro/document/2"]').items():
+            self.crawl(each.attr.href, callback=self.detail_page)
+    
+    
+    @config(priority=2)
+    def detail_page(self, response):
+        content = response.doc('.document .content')
+        inputTable = content('table').eq(0)
+        outputTable = content('table').eq(1)
+        inputField = {
+            "keys": ['name','type','required','describe'],
+            "values": ['名称','类型','必选','描述']
+        }
+        outputField = {
+            "keys": ['name','type','describe'],
+            "values": ['名称','类型','描述']
+        }
+        inputArr = getTable2tushare(inputTable, inputField)
+        outputArr = getTable2tushare(outputTable, outputField)
+        apiKey = getStrBetween(content('p').eq(0).text(),'接口：','\n描述：')
+        tsApi = {
+            apiKey: {
+                "api": apiKey,
+                "name": content('#-').text(),
+                "describe": getStrBetween( content('p').eq(0).text(), '\n描述：' ),
+                "inputField": inputField,
+                "input": inputArr,
+                "outputField": outputField,
+                "output": outputArr
+            }
+        }
+
+        print('------')
+        return {
+            "url": response.url,
+            "title": response.doc('title').text(),
+            "api": tsApi
+        }
+    
--- src/py/queryDataFn.py
+++ src/py/queryDataFn.py
@@ -6,6 +6,67 @@ ts.set_token('667cebb835b7c7c4b92adf730806e90a2b7849ae573eb3a24f1c3a7a')
 #初始化pro接口
 pro = ts.pro_api()
 
+'''
+页面转对象
+var subStrBetween = function(str, startStr, endStr){
+    var sLength = startStr.length, eLength = endStr.length;
+    var startIndex = str.indexOf(startStr) + sLength;
+    var endIndex = str.indexOf(endStr) - startIndex;
+    var result = str.substr(startIndex, endIndex);
+    return result;
+};
+var content = $('.content').eq(0);
+var html = content.html();
+var obj = {
+    apiName:{
+        api: subStrBetween( html, '接口：', '<br>描述：' ),
+        describe: subStrBetween( html, '<br>描述：', '</p>' )
+
+    }
+};
+obj;
+'''
+
+tsApi = {
+    #指数
+    'indexApi':{
+        'index_basic':{
+            'api': 'index_basic', 'describe': '获取指数基础信息。',
+            'input': [
+                {'name': 'market', 'type': 'str', 'required': 'Y', 'describe': '交易所或服务商'},
+                {'name': 'publisher', 'type': 'str', 'required': 'N', 'describe': '发布商'},
+                {'name': 'category', 'type': 'str', 'required': 'N', 'describe': '指数类别'}
+            ],
+            'output':[
+                {'name': 'ts_code', 'type': 'str', 'describe': 'TS代码'},
+                {'name': 'name', 'type': 'str', 'describe': '简称'},
+                {'name': 'name', 'type': 'str', 'describe': '指数全称'},
+                {'name': 'market', 'type': 'str', 'describe': '市场'},
+                {'name': 'publisher', 'type': 'str', 'describe': '发布方'},
+                {'name': 'index_type', 'type': 'str', 'describe': '指数风格'},
+                {'name': 'category', 'type': 'str', 'describe': '指数类别'},
+                {'name': 'base_date', 'type': 'str', 'describe': '基期'},
+                {'name': 'base_point', 'type': 'float', 'describe': '基点'},
+                {'name': 'list_date', 'type': 'str', 'describe': '发布日期'},
+                {'name': 'weight_rule', 'type': 'str', 'describe': '加权方式'},
+                {'name': 'desc', 'type': 'str', 'describe': '描述'},
+                {'name': 'exp_date', 'type': 'str', 'describe': '终止日期'}
+            ],
+            'map':{
+                'market':{
+                    'MSCI': 'MSCI指数', 'CSI': '中证指数', 'SSE': '上交所指数', 'SZSE': '深交所指数', 'CICC': '中金所指数', 'SW': '申万指数', 'OTH': '其他指数'
+                },
+                'category':[
+                    '主题指数','规模指数','策略指数','风格指数','综合指数','成长指数','价值指数','有色指数','化工指数','能源指数',
+                    '其他指数','外汇指数','基金指数','商品指数','债券指数','行业指数','贵金属指数','农副产品指数','软商品指数','油脂油料指数',
+                    '非金属建材指数','煤焦钢矿指数','谷物指数','一级行业指数','二级行业指数','三级行业指数'
+                ] 
+            }
+        }
+    }
+}
+
+
 #查询指数基础信息
 def index_basic(market, publisher='', category=''):
     return pro.index_basic(market=market, publisher=publisher, category=category)
